868fb95daf1d128c4b7d10f74bc91a636a0226be
zhj <zhengjingytz@126.com>
2017-08-23 12:00:44 +0800

    init directory

---
+++ app/src/main/java/cos/jingzheng/gittestwidget/MainActivity.java
@@ -0,0 +1,15 @@
+package cos.jingzheng.gittestwidget;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+    }
+}
